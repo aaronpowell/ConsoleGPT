ba0ed5d3a2c4354d08ec3469780e0278721d50e8
Aaron Powell <me@aaron-powell.com>
2023-10-25 12:00:46 +1100

    Adding streaming example

---
+++ ConsoleGPT.OpenAISdk.Streaming/ConsoleGPTService.cs
@@ -0,0 +1,102 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using ConsoleGPT;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+internal class ConsoleGPTService : IHostedService
+{
+    private readonly IHostApplicationLifetime lifetime;
+    private readonly OpenAIClient openAIClient;
+    private readonly IOptions<Settings> settings;
+
+    public ConsoleGPTService(IHostApplicationLifetime lifetime, OpenAIClient openAIClient, IOptions<Settings> settings)
+    {
+        this.lifetime = lifetime;
+        this.openAIClient = openAIClient;
+        this.settings = settings;
+    }
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Settings s = settings.Value;
+
+        bool goodbye = false;
+        ChatCompletionsOptions completionsOptions = new()
+        {
+            MaxTokens = s.MaxTokens,
+            Temperature = s.Temperature,
+            FrequencyPenalty = s.FrequencyPenalty,
+            PresencePenalty = s.PresencePenalty,
+            Messages =
+            {
+                new(ChatRole.System, s.SystemPrompt)
+            }
+        };
+
+        WriteAssistantMessage("Beep, boop, I'm .DotNetBot and I'm here to help. If you're done say goodbye.", newLine: true);
+        while (!goodbye)
+        {
+            string? input = await Console.In.ReadLineAsync(cancellationToken);
+
+            if (string.IsNullOrEmpty(input))
+            {
+                continue;
+            }
+
+            if (input.Equals("goodbye", StringComparison.OrdinalIgnoreCase))
+            {
+                goodbye = true;
+                continue;
+            }
+
+            completionsOptions.Messages.Add(new(ChatRole.User, input));
+
+            Response<StreamingChatCompletions> completions = await openAIClient.GetChatCompletionsStreamingAsync(s.Model, completionsOptions, cancellationToken);
+
+            IAsyncEnumerable<StreamingChatChoice> choices = completions.Value.GetChoicesStreaming(cancellationToken);
+
+            await foreach (StreamingChatChoice choice in choices)
+            {
+                IAsyncEnumerable<ChatMessage> message = choice.GetMessageStreaming(cancellationToken);
+
+                StringBuilder completeMessage = new();
+
+                await foreach (ChatMessage m in message)
+                {
+                    string content = m.Content;
+
+                    WriteAssistantMessage(content, newLine: false);
+
+                    completeMessage.Append(content);
+
+                    // Simulate some delay in the network
+                    await Task.Delay(200, cancellationToken);
+                }
+                Console.WriteLine();
+
+                completionsOptions.Messages.Add(new(ChatRole.Assistant, completeMessage.ToString()));
+
+            }
+        }
+
+        lifetime.StopApplication();
+    }
+
+    private static void WriteAssistantMessage(string content, bool newLine = true)
+    {
+        ConsoleColor textColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+        if (newLine)
+        {
+            Console.Out.WriteLine(content);
+        }
+        else
+        {
+            Console.Out.Write(content);
+        }
+        Console.ForegroundColor = textColor;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
---
+++ ConsoleGPT.OpenAISdk.Streaming/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using ConsoleGPT;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+hostBuilder.ConfigureAppConfiguration((builder) => builder
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true, true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>());
+
+hostBuilder.ConfigureServices((context, services) =>
+{
+    services.Configure<Settings>(context.Configuration.GetSection("settings"));
+
+    services.AddSingleton(provider =>
+    {
+        Settings settings = provider.GetRequiredService<IOptions<Settings>>().Value;
+
+        OpenAIClient client = settings.Type == OpenAIType.Azure
+            ? new OpenAIClient(new Uri(settings.Endpoint!), new AzureKeyCredential(settings.Key))
+            : new OpenAIClient(settings.Key!);
+
+        return client;
+    });
+
+    services.AddHostedService<ConsoleGPTService>();
+});
+
+await hostBuilder.Build().RunAsync();
